80e386be800ceab6a9d000370a8de5c9fc5fb027
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-27 17:00:43 +0300

    type annoted func sum_list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+file 5-sum_list.py
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''
+    type-annotated func, takes list(input_list(floats)) args returns sum(float)
+    '''
+    return sum(input_list)
