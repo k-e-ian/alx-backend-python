2afa3a7e2dfa53681b37ba342d8ea8d0555d93c7
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-27 16:33:54 +0300

    type-annoted function concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+file 1-concat.py
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''type-annotated function concat, takes str(str1 and str2) returns str'''
+    return str1 + str2
