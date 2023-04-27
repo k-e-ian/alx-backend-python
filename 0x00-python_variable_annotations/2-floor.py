d9245ecee354573033d22b4b02794b6a811d0c2c
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-27 16:37:59 +0300

    type-annoted function floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+file 2-floor.py
+'''
+import math
+
+
+def floor(n: float) -> int:
+    '''type-annotated function floor, takes float(n) returns floor of float'''
+    return math.floor(n)
