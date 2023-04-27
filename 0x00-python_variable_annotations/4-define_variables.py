061afc055d22eb282a419b6467ee2ded331ffcf2
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-27 16:50:32 +0300

    define and annotae variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+file 4-define_variables.py
+'''
+
+'''
+annotation and definition of variables
+'''
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
