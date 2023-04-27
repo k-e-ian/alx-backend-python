6bcd1fbd09630c6a12262be38171f93ce74ab538
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-27 16:42:00 +0300

    type-annoted function to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+file 3-to_str.py
+'''
+
+
+def to_str(n: float) -> str:
+    '''type-annotated function floor, takes float(n) returns floor of float'''
+    return str(n)
