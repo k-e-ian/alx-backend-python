8d4a9453124f32417ab64d97fc2e3d69a704a124
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-27 16:27:29 +0300

    typed-annoted function add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+file 0-add.py
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''type-annotated function add, takes float(a and b) returns sum float'''
+    return a + b
